5a63a73f755f44764bdcd99348434f5939ce4f56
Espen Trydal <espen@fastmail.com>
2017-12-09 17:30:51 +0100

    customizing

--- sample/prelude-modules.el
+++ sample/prelude-modules.el
@@ -1,9 +1,9 @@
 ;;; Uncomment the modules you'd like to use and restart Prelude afterwards
 
 (require 'prelude-erc) ;; Emacs IRC client
-(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
-;;(require 'prelude-helm) ;; Interface for narrowing and search
-;;(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
+;;(require 'prelude-ido) ;; Super charges Emacs completion for C-x C-f and more
+(require 'prelude-helm) ;; Interface for narrowing and search
+(require 'prelude-helm-everywhere) ;; Enable Helm everywhere
 (require 'prelude-company)
 (require 'prelude-key-chord) ;; Binds useful features to key combinations
 ;; (require 'prelude-mediawiki)
@@ -13,22 +13,22 @@
 (require 'prelude-c)
 (require 'prelude-clojure)
 ;; (require 'prelude-coffee)
-;; (require 'prelude-common-lisp)
+(require 'prelude-common-lisp)
 ;; (require 'prelude-css)
 (require 'prelude-emacs-lisp)
 ;; (require 'prelude-erlang)
 ;; (require 'prelude-elixir)
-;; (require 'prelude-go)
+(require 'prelude-go)
 ;; (require 'prelude-haskell)
 (require 'prelude-js)
-;; (require 'prelude-latex)
+(require 'prelude-latex)
 (require 'prelude-lisp)
 ;; (require 'prelude-ocaml)
 (require 'prelude-org) ;; Org-mode helps you keep TODO lists, notes and more
-(require 'prelude-perl)
-;; (require 'prelude-python)
+;;(require 'prelude-perl)
+(require 'prelude-python)
 ;; (require 'prelude-ruby)
-;; (require 'prelude-rust)
+(require 'prelude-rust)
 ;; (require 'prelude-scala)
 (require 'prelude-scheme)
 (require 'prelude-shell)
